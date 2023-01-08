ad145a7d846b126069407f8b1711783c175ff7b3
dinger1986 <daniel.lamb@flonix.co.uk>
2023-01-08 23:56:40 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,10 @@ sudo chmod +x iRedMail.sh
 rm -rf ../${iRMLATEST}.tar.gz
 ./iRedmail.sh
 
+echo "Take a note of your settings now"
+sudo amavisd-new showkeys
+read -p "Press [Enter] once you have copied this output..."
+
 echo "Installing latest version of iRedMail Admin"
 admrel=$(curl https://github.com/iredmail/iRedAdmin/tags -s | grep 'tool-tip for="toggle-commit-'| awk '{print substr($3, 20, 3) }')
 iRMADM=$(echo $admrel | awk '{print substr($1, 1) }')
@@ -25,6 +29,10 @@ sudo chmod +x upgrade_iredadmin.sh
 rm -rf ../${iRMADM}.tar.gz
 ./upgrade_iredadmin.sh
 
+echo "Add DKIM Key to your DNS as a TXT Record"
+sudo amavisd-new showkeys
+read -p "Press [Enter] once you have copied this output..."
+
 echo "Installing SSL certs and rebooting"
 hostname=$(hostname -f)
 sudo apt install -y certbot python3-certbot-nginx
@@ -33,4 +41,4 @@ sudo mv /etc/ssl/certs/iRedMail.crt{,.bak}
 sudo mv /etc/ssl/private/iRedMail.key{,.bak}
 sudo ln -sf /etc/letsencrypt/live/mail.flonix.ml/fullchain.pem /etc/ssl/certs/iRedMail.crt
 sudo ln -sf  /etc/letsencrypt/live/mail.flonix.ml/privkey.pem /etc/ssl/private/iRedMail.key
-reboot
+sudo reboot
